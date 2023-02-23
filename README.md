e44151b408edf11c36b9fb5d2beb95ae00949967
Bui Thanh Xuan <buithanhxuan2261@gmail.com>
2023-02-23 08:55:35 +0700

    Add files via upload
    firstupdate

---
+++ README.md
@@ -0,0 +1,6 @@
+# BraveReleases
+Cập nhật các phiên bản
+
+New: https://braveupdate.netlify.app
+
+Old: https://brave-update.vercel.app
---
+++ script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://brave.com/static-assets/images/optimized/desktop-hero-screenshot@1x.png" alt="" />'
+  title.innerHTML = 'Đây là server update Brave Tool'
+  excerpt.innerHTML =
+    'Brave Tool sẽ thường xuyên được cập nhật dựa trên bản cập nhật của trình duyệt Brave.'
+  profile_img.innerHTML =
+    '<img src="https://xsgames.co/randomusers/avatar.php?g=female" alt="" />'
+  name.innerHTML = 'xuan2261'
+  date.innerHTML = 'March 1, 1993'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
